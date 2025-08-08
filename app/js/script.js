93af90e8b4fc65cb14794637f179e9712b9b9dd2
Líderes Conscientes <93500932+CODABits@users.noreply.github.com>
2025-08-07 21:25:55 -0500

    Add files via upload

---
+++ app/js/script.js
@@ -0,0 +1,30 @@
+function togglePopup(popupId) {
+    var popup = document.getElementById(popupId);
+    popup.style.display = popup.style.display === 'block' ? 'none' : 'block';
+}
+
+function closePopup(popupId) {
+    var popup = document.getElementById(popupId);
+    popup.style.display = 'none';
+}
+
+
+
+
+function toggleAuthorPopup() {
+    const popup = document.getElementById('author-popup');
+    popup.style.display = popup.style.display === 'block' ? 'none' : 'block';
+}
+
+function closeAuthorPopup() {
+    document.getElementById('author-popup').style.display = 'none';
+}
+
+
+// Cualquier código existente que tengas
+console.log("Este es el código existente.");
+
+
+
+
+
